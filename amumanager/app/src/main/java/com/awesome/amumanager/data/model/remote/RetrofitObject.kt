e40e094c89fee0d969c15a3fc305eebecbd7d6f0
swc4327 <swc4327@naver.com>
2021-08-18 15:55:03 +0900

    retrofitobject refactoring

--- amumanager/app/src/main/java/com/awesome/amumanager/data/model/remote/RetrofitObject.kt
+++ amumanager/app/src/main/java/com/awesome/amumanager/data/model/remote/RetrofitObject.kt
@@ -19,25 +19,4 @@ object RetrofitObject {
     val promotionService : PromotionService = retrofit.create(PromotionService::class.java)
     val signUpService : SignUpService = retrofit.create(SignUpService::class.java)
     val getClientService : GetClientService = retrofit.create(GetClientService::class.java)
-
-    //val getStoreService: GetStoreService = retrofit.create(GetStoreService::class.java)
-    //val addStoreService : AddStoreService = retrofit.create(AddStoreService::class.java)
-    //val addMenuService : AddMenuService = retrofit.create(AddMenuService::class.java)
-    //val addPromotionService : AddPromotionService = retrofit.create(AddPromotionService::class.java)
-
-    //val getMenuService : GetMenuService = retrofit.create(GetMenuService::class.java)
-    //val getPromotionService : GetPromotionService = retrofit.create(GetPromotionService::class.java)
-    //val getReserveService : GetReserveService = retrofit.create(GetReserveService::class.java)
-    //val getReviewService : GetReviewService = retrofit.create(GetReviewService::class.java)
-    //val reviewFilteringService : ReviewFilteringService = retrofit.create(ReviewFilteringService::class.java)
-
-    //val confirmReserveService : ConfirmReserveService = retrofit.create(ConfirmReserveService::class.java)
-    //val cancelReserveService : CancelReserveService = retrofit.create(CancelReserveService::class.java)
-    //val completeReserveService : CompleteReserveService = retrofit.create(CompleteReserveService::class.java)
-    //val endPromotionService : EndPromotionService = retrofit.create(EndPromotionService::class.java)
-    //val deleteMenuService : DeleteMenuService = retrofit.create(DeleteMenuService::class.java)
-    //val deleteStoreService : DeleteStoreService = retrofit.create(DeleteStoreService::class.java)
-    //val getConfirmedReserveService : GetConfirmedReserveService = retrofit.create(GetConfirmedReserveService::class.java)
-    //val openStoreService : OpenStoreService = retrofit.create(OpenStoreService::class.java)
-    //val closeStoreService : CloseStoreService = retrofit.create(CloseStoreService::class.java)
 }
